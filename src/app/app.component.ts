79f54922615e67425efed54cd62ea89399d00fe0
ekene <ekeneuduike@gmail.com>
2024-06-13 23:30:24 +0100

    installed some nice dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet,  } from '@angular/router';
-// import {MatIconModule} from '@angular/material/icon';
-// import {MatDividerModule} from '@angular/material/divider';
-// import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterLink, RouterLinkActive,
-    // MatIconModule,MatButtonModule,MatDividerModule
+    MatIconModule,MatButtonModule,MatDividerModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
