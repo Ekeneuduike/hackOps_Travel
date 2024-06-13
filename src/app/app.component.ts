77d77bf15e2d371e122542d3673f0c0880f2a31f
ekene <ekeneuduike@gmail.com>
2024-06-13 23:19:45 +0100

    created navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet,  } from '@angular/router';
+// import {MatIconModule} from '@angular/material/icon';
+// import {MatDividerModule} from '@angular/material/divider';
+// import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive,
+    // MatIconModule,MatButtonModule,MatDividerModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipeops_hackkerton_frontend';
+
+  title = 'travel_buddy_frontend';
+
+  toggle() {
+    let doc = document.getElementById('tog-menu');
+    doc?.classList.toggle('hidden');
+    doc?.addEventListener('click',()=> {
+      if (doc) doc.classList.add('hidden');
+    },{once:true})
+    console.log('hello');
+  }
 }
